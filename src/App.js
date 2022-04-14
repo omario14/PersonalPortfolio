d98561ec686c4b0056e086247c2f62b282ee7f3f
omario14 <omar.benamor@esprit.tn>
2022-04-14 16:11:16 +0100

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+
+import Nav from './pages/Navigation/nav';
+import Home from './pages/home';
+import Footer from './pages/Footer/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <Nav/>
+    <Home/>
+    <Footer/>
+  </div>
+    
   );
 }
 
