cabfe4c63fd4f029d71c9f9b6e442d7934b260cf
omario14 <omarb1414a@gmail.com>
2023-02-17 17:34:56 +0100

    cio

--- src/pages/Navigation/Nav.jsx
+++ src/pages/Navigation/Nav.jsx
@@ -116,14 +116,14 @@ export default function Nav() {
                                                 <li class="yek-social__item">
                                                     <a class="yek-social__link" href="https://linkedin.com/in/omarbenamorlin" target="_blank">
                                                         <span class="yek-social__button yek-social__button--gmail">
-                                                            <i class="yek-social__icon yek-social__icon--gmail linkedinIcon"></i>
+                                                            <i class="linkedinIcon yek-social__icon yek-social__icon--gmail "></i>
                                                         </span>
                                                     </a>
                                                 </li>
                                                 <li class="yek-social__item">
                                                     <a class="yek-social__link" href="https://github.com/omario14" target="_blank">
                                                         <span class="yek-social__button yek-social__button--github">
-                                                            <i class="yek-social__icon  yek-social__icon--codepen githubIcon"></i>
+                                                            <i class="githubIcon yek-social__icon  yek-social__icon--codepen "></i>
                                                         </span>
                                                     </a>
                                                 </li>
